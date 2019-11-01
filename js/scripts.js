56a268c36d353b00499ba48bcfd389fda068af97
Casey Antoine <crantoine@gmail.com>
2019-11-01 12:00:49 -0700

    add styling and basic functionality.

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// Business Logic
+let list = [];
+
+function range(number) {
+    for(i=0; i < userInput; i++){
+      list.push(i);
+    }
+}
+
+
+
+
+// User Interface
+$(document).ready(function() {
+
+var userInput = parseInt($("#userInput").val());
+
+
+
+});

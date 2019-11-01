58c40ffb3ea0eecbc9e6e93a00114a12e486a8bd
Casey Antoine <crantoine@gmail.com>
2019-11-01 12:30:33 -0700

    updated functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
 // Business Logic
 let list = [];
 
-function range(number) {
+function range(userInput) {
+  if (userInput === "") {
+    console.log("Sorry Dave, you have to pick a number 1-100");
+  } else if (Number.isInteger(userInput) != true) {
+    console.log("We don't accept text, you have to enter a number value 1-100.");
+  }
+
     for(i=0; i < userInput; i++){
       list.push(i);
     }
+    list.push(userInput);
+
+    console.log(list);
+
+    list.length = 0;
+
+    console.log(list + " has been cleared.");
 }
 
 
@@ -13,8 +26,15 @@ function range(number) {
 // User Interface
 $(document).ready(function() {
 
-var userInput = parseInt($("#userInput").val());
+  $(".roboForm").submit(function(ev) {
+    ev.preventDefault();
+
+    var userInput = parseInt($("#userInput").val());
+
+    range(userInput);
 
 
 
+
+  });
 });
